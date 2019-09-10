069968b95ae50e1e115ebf68ecdc437cbb009c39
WangJing <subaoing@163.com>
2019-09-10 21:10:57 +0800

    再次

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-/**vscode提交 */
+/**vscode提交咳咳  */
 Vue.config.productionTip = false
 
 new Vue({
